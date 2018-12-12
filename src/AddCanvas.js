7099b8f2f6e7f28599806246cc0ed2f180a03183
qinqinlv <1711814259@qq.com>
2018-12-12 13:45:15 +0800

    test by html5 canvas tag
    display an image in canvas when crossOrigin is set or not

---
+++ src/AddCanvas.js
@@ -0,0 +1,89 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+
+import "./styles.css";
+
+class AddCropper extends React.Component {
+  state = {
+    urlForCrop: "",
+    loading: true,
+    visible: false,
+    // testUrl是没有设置Cors响应头的跨域地址
+    testUrl:
+      "https://image.synnex.com/ec/CLOUDSolv/UAT/reseller/resources/resellerLogo/72684_logo-solvs_20180829021645964_4SeXP.png",
+    // testUrl2是设置了Cors响应头的跨域地址
+    testUrl2:
+      "https://image.synnex.com/p1/us/uat/publicImages/storefront/US/19783b9c-b01e-4673-a3b2-fc4b395cb456.png"
+  };
+  componentDidMount = () => {
+    this.addCanvas();
+  };
+  // 这里其实还有一个疑惑，就是在MDN上说，尽管没有Cors授权也可以在canvas中使用图像，但是这样会污染canvas画布。
+  // 但是现在使用cropper组件的时候，如果传入的是一个没有Cors授权的url,根本就显示不出来。下面使用h5中的canvas标签重新测试。
+  // 结论是 ：
+  // 1. 上面的结论是正确的，我使用cropper组件的时候，如果图片地址是未设置Cors的跨域地址，且不添加crossOrigin='anonymous',
+  // 不会显示出图片； 但是直接手写canvas是可以展示图片的。
+  // 2. 只要img设置了crossOrigin='anonymous'，报ajax请求错误。由于图片根本进不了onload事件，所以canvas中根本不会渲染图片；
+  // 3. 如果设置了onerror事件后还是drawImage这个跨域且无Cors授权的图片，那么会报错当前图片损坏；
+  addCanvas = () => {
+    console.log("进入了自定义canvas代码中");
+    let currentUrl = this.state.testUrl;
+    // const currentUrl = this.state.testUrl2;
+    let cvs = document.getElementById("currentCanvas");
+    cvs.width = 320;
+    cvs.height = 60;
+    var ctx = cvs.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.lineTo(320, 0);
+    ctx.lineTo(320, 60);
+    ctx.lineTo(0, 60);
+    ctx.closePath();
+    ctx.stroke();
+    let img = new Image();
+    img.src = currentUrl;
+    img.crossOrigin = "anonymous";
+    img.onload = function() {
+      ctx.drawImage(
+        img,
+        0,
+        0,
+        img.naturalWidth,
+        img.naturalHeight,
+        2,
+        2,
+        img.naturalWidth,
+        img.naturalHeight
+      );
+      console.log("结束执行自定义canvas中的逻辑");
+    };
+    img.onerror = function() {
+      console.log("原生canvas中跨域请求图片失败");
+      // index.js? [sm]:154 Uncaught DOMException: Failed to execute 'drawImage' on 'CanvasRenderingContext2D': The HTMLImageElement provided is in the 'broken' state.
+      // ctx.drawImage(
+      //   img,
+      //   0,
+      //   0,
+      //   img.naturalWidth,
+      //   img.naturalHeight,
+      //   2,
+      //   2,
+      //   img.naturalWidth,
+      //   img.naturalHeight
+      // );
+      console.log("结束执行自定义canvas中的逻辑");
+    };
+  };
+
+  render() {
+    let currentUrl = this.state.testUrl;
+    // let currentUrl = this.state.testUrl2;
+    return (
+      <div className="originalCanvas">
+        <canvas id="currentCanvas" />
+      </div>
+    );
+  }
+}
+
+export default AddCropper;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Spin, Modal } from "antd";
+import AddCanvas from "./AddCanvas";
 import Cropper from "react-cropper";
 import "cropperjs/dist/cropper.css";
 import "antd/dist/antd.css";
@@ -12,20 +13,22 @@ class App extends React.Component {
     urlForCrop: "",
     loading: true,
     visible: false,
+    // testUrl是没有设置Cors响应头的跨域地址
     testUrl:
       "https://image.synnex.com/ec/CLOUDSolv/UAT/reseller/resources/resellerLogo/72684_logo-solvs_20180829021645964_4SeXP.png",
+    // testUrl2是设置了Cors响应头的跨域地址
     testUrl2:
       "https://image.synnex.com/p1/us/uat/publicImages/storefront/US/19783b9c-b01e-4673-a3b2-fc4b395cb456.png"
   };
   componentDidMount = () => {
-    this.estimateImageValidOrNot();
+    // this.estimateImageValidOrNot();
   };
 
   estimateImageValidOrNot = () => {
     this.setState({
       loading: true
     });
-    let currentUrl = this.state.testUrl;
+    let currentUrl = this.state.testUrl1;
 
     new Promise((res, rej) => {
       let imageObj = new Image(); // eslint-disable-line
@@ -33,11 +36,16 @@ class App extends React.Component {
       imageObj.src = currentUrl;
       imageObj.onload = () => {
         console.log("这里是否请求成功？");
+        // 如果这里的src是一个没有设置CORS的跨域图片，不管上面的crossOrigin
+        // 属性有没有设置，这个图片还是可以被成功load的。
+        // 结论是： 使用img标签在页面上可以渲染任意图片，不管是否跨域，不管img是否
+        // 设置了crossOrigin属性，或者图片url接口设置了Cors响应头
         return res(imageObj);
       };
       imageObj.onerror = () => {
         console.log("这里是够请求失败？");
-        // 这里执行了的，表明当设置了crossOrigin的时候，获取没有设置Cors的图片是失败的
+        // 当设置了crossOrigin的时候，获取没有设置Cors的图片会失败，报ajax请求失败。
+        // 但是如果是用img装载该图片，还是可以显示在页面中的。
         return rej(imageObj);
       };
     })
@@ -70,7 +78,10 @@ class App extends React.Component {
   };
 
   cropBoxRenderReady = () => {
-    console.log("这个ready函数在图片没有读出来的时候依旧是被调用了的？");
+    // console.log("这个ready函数在图片没有读出来的时候依旧是被调用了的？");
+    // 结果显示这个ready函数在如下情况下都会执行：
+    // canvas中放置有Cors设置的跨域地址
+    // canvas中放置没有Cors设置的跨域地址
     this.setState({
       loading: false
     });
@@ -101,35 +112,18 @@ class App extends React.Component {
       visible: true
     });
   };
-
+  // 1. 在使用cropper组件的时候，如果没有设置cropper组件中的crossOrigin为anonymous，如果图片跨域且无Cors，console中还是显示使用了
+  // ajax的方式请求;说明在cropper内部，还是打开了image的crossOrigin的。
   render() {
-    console.log("这个是loading的值？", this.state.loading);
-    let currentUrl = this.state.testUrl2;
+    let currentUrl = this.state.testUrl;
+    // let currentUrl = this.state.testUrl2;
     return (
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
         <img src={currentUrl} alt="" />
         <input type="button" value="打开crop框" onClick={this.openModal} />
-        <Cropper
-          src={currentUrl}
-          // crossOrigin={"Anonymous"}
-          className={"cropContainer"}
-          aspectRatio={292 / 56}
-          autoCrop={true}
-          minCropBoxWidth={292}
-          minCropBoxHeight={56}
-          cropBoxMovable={false}
-          cropBoxResizable={false}
-          guides={true}
-          modal={true}
-          center={false}
-          dragMode={"move"}
-          toggleDragModeOnDblclick={false}
-          ref={this.createCropperRef}
-          ready={this.cropBoxRenderReady}
-          crop={this.cropLogo}
-        />
+
         <Modal
           title="Basic Modal"
           destroyOnClose={true}
